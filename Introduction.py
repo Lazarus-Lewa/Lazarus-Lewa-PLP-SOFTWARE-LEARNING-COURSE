91b075ff488d2d849d8e7d742e532f99538a3918
Lazarus Lewa <lazaruslewa@gmail.com>
2023-12-08 16:09:34 +0300

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,24 @@
+# Datatypes in python
+
+#Numerals
+num=24 #integer(int)
+pie=3.142 #float
+# double-3.14246
+
+#End of numerals
+
+#String
+wish="Happy birthday Uhuru Kenyatta"
+Age="19"
+num2="24"
+#End of string
+
+
+#Collection of items....array
+#list,tuple,range
+#List
+crushies=["Rose","Winnie","Murife","Monica",254,(1,2,3)]#square brackets
+#Accessing items in the list
+print(crushies[1])
+print("Hi",crushies[1])
+
